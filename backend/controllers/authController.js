23cecaffe84bdd7ed6d565e561a0fbf04f9f97b1
Kaiki <kaikiandrade64@gmail.com>
2025-11-08 14:18:58 -0300

    adicionado a rota de login com criptografia

---
+++ backend/controllers/authController.js
@@ -0,0 +1,34 @@
+import User from "../models/user.js";
+import bcrypt from "bcryptjs";
+
+export const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({ name, email, password: hashedPassword });
+        await user.save();
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid password" });
+        }
+
+        res.status(200).json({ message: "Login successful", user });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
